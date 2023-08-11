4aae98ea20b204cf3188b14fd4ad05cffb9037ab
aguramirez <agustinramirez58@gmail.com>
2023-08-11 19:23:20 -0300

    post en controller

--- src/main/java/com/agustin/backend/usersapp/backendusersapp/controllers/UserController.java
+++ src/main/java/com/agustin/backend/usersapp/backendusersapp/controllers/UserController.java
@@ -1,10 +1,15 @@
 package com.agustin.backend.usersapp.backendusersapp.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,8 +29,17 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public User show(@PathVariable Long id){
-        return service.findById(id).orElseThrow();
+    public ResponseEntity<?> show(@PathVariable Long id){
+        Optional<User> userOptional = service.findById(id);
+
+        if(userOptional.isPresent()){
+            return ResponseEntity.ok(userOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
     }
     
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody User user){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(user));
+    }
 }
