2e5552bd4257ee4e5170ac919cb3301df58c7e37
aguramirez <agustinramirez58@gmail.com>
2023-09-15 15:35:09 -0300

    JWT209

--- src/main/java/com/agustin/backend/usersapp/backendusersapp/services/JpaUserDetailsService.java
+++ src/main/java/com/agustin/backend/usersapp/backendusersapp/services/JpaUserDetailsService.java
@@ -2,7 +2,9 @@ package com.agustin.backend.usersapp.backendusersapp.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -10,22 +12,33 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.agustin.backend.usersapp.backendusersapp.repositories.UserRepository;
 
 @Service
 public class JpaUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private UserRepository repository;
+    
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        if (!username.equals("admin")) {
+        Optional<com.agustin.backend.usersapp.backendusersapp.models.entities.User> o = repository.findByUsername(username); 
+
+        if (!o.isPresent()) {
             throw new UsernameNotFoundException(String.format("Username %s no existe en el sistema!", username));
         }
+
+        com.agustin.backend.usersapp.backendusersapp.models.entities.User user = o.orElseThrow();
 
         List<GrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
 
-        return new User(username,
-                "$2a$10$DOMDxjYyfZ/e7RcBfUpzqeaCs8pLgcizuiQWXPkU35nOhZlFcE9MS",
+        return new User(user.getUsername(),
+                user.getPassword(),
                 true,
                 true,
                 true,
