84ce769e89c5cd71def7d1196081acfd79773c1a
aguramirez <agustinramirez58@gmail.com>
2023-09-27 18:51:57 -0300

    226JWT

---
+++ src/main/java/com/agustin/backend/usersapp/backendusersapp/models/dto/mapper/DtoMapperUser.java
@@ -0,0 +1,15 @@
+package com.agustin.backend.usersapp.backendusersapp.models.dto.mapper;
+
+public class DtoMapperUser {
+    
+    private static DtoMapperUser mapper;
+    
+    private DtoMapperUser(){
+
+    }
+
+    public static DtoMapperUser getInstance(){
+        mapper = new DtoMapperUser();
+        return mapper;
+    }
+}
