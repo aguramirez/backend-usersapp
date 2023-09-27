2938c8060836f7ba3b7733b85c79b8180baa5b19
aguramirez <agustinramirez58@gmail.com>
2023-09-27 18:37:17 -0300

    224JWT

--- src/main/java/com/agustin/backend/usersapp/backendusersapp/auth/SpringSecurityConfig.java
+++ src/main/java/com/agustin/backend/usersapp/backendusersapp/auth/SpringSecurityConfig.java
@@ -35,6 +35,11 @@ public class SpringSecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http.authorizeHttpRequests()
         .requestMatchers(HttpMethod.GET, "/users").permitAll()
+        .requestMatchers(HttpMethod.GET, "/users/{id}").hasAnyRole("USER", "ADMIN")
+        .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
+        .requestMatchers("/users/**").hasRole("ADMIN")
+        // .requestMatchers(HttpMethod.DELETE, "/users/{id}").hasRole("ADMIN")
+        // .requestMatchers(HttpMethod.PUT, "/users/{id}").hasRole("ADMIN")
         .anyRequest().authenticated()
         .and()
         .addFilter(new JwtAuthenticationFilter(authenticationConfiguration.getAuthenticationManager()))
